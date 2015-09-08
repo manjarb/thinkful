4e7566c9f2321be5ab3fca9a8840d20fe85a0c6f
Varis Darasirikul <manjarb4@gmail.com>
2015-09-09 03:03:55 +0700

    wait staff

--- Waitstaff_Calculator/js/waitstaff_calculator_form.js
+++ Waitstaff_Calculator/js/waitstaff_calculator_form.js
@@ -1,30 +1,43 @@
-angular.module('madLibs', ['ngMessages'])
+angular.module('waitStaff', [])
     .controller('MyCtrl', function($scope) {
-        // $scope.female_name = 'female_name';
-        // $scope.job_title='job_title';
-        // $scope.tedious_task='tedious_task';
-        // $scope.dirty_task='dirty_task';
-        // $scope.female_name2='female_name2';
-        // $scope.celebrity='celebrity';
-        // $scope.useless_skill='useless_skill';
-        // $scope.adjective='adjective';
-        // $scope.obnoxiuous_celbertity='obnoxiuous_celbertity';
-        // $scope.dirty_task='dirty_task';
+
+        $scope.mealCount = 0;
+        $scope.totalTip = 0;
+
+        $scope.submitForm = function(){
+          alert('submit test')
+
+          $scope.subTotal = $scope.baseMeal + (($scope.taxRate / 100) * $scope.baseMeal);
+          $scope.tip = (($scope.tipPercent / 100) * $scope.subTotal);
+          $scope.totalPrice = $scope.subTotal + $scope.tip;
+
+          $scope.totalTip = $scope.totalTip + $scope.tip;
+
+          $scope.mealCount = $scope.mealCount + 1;
+
+          $scope.avgTips = $scope.totalTip / $scope.mealCount;
+        }
+
+        $scope.resetAll = function() {
+
+          $scope.mealCount = 0;
+          $scope.totalTip = 0;
+          $scope.baseMeal = 0;
+          $scope.taxRate = 0;
+          $scope.tipPercent = 0;
+
+          $scope.subTotal = "";
+          $scope.tip = "";
+          $scope.totalPrice = "";
+          $scope.totalTip = 0;
+
+          $scope.waitStaffForm.$setPristine();
+          $scope.waitStaffForm.$setUntouched();
+        };
 
         $scope.reset = function() {
-          $scope.female_name = "";
-          $scope.job_title = "";
-          $scope.tedious_task = "";
-          $scope.dirty_task = "";
-          $scope.female_name2 = "";
-          $scope.celebrity = "";
-          $scope.useless_skill = "";
-          $scope.adjective = "";
-          $scope.obnoxiuous_celbertity = "";
-          $scope.dirty_task = "";
-          $scope.huge_number = "";
-
-          $scope.myForm.$setPristine();
-          $scope.myForm.$setUntouched();
+          $scope.baseMeal = "";
+          $scope.taxRate = "";
+          $scope.tipPercent = "";
         };
     });
