5dd8e0acd0c5fb55db02ecfecc1bb43ac2de87fa
Varis Darasirikul <manjarb4@gmail.com>
2015-09-17 18:54:53 +0700

    aaa

---
+++ testing_angular/app/components/helloService.js
@@ -0,0 +1,12 @@
+angular.module('HelloModule', [])
+	.factory('uppercaseService', function() {
+        return function() {
+            return 'hello';
+        }
+    })
+    .factory('helloService', function(uppercaseService) {
+        return function() {
+            return uppercaseService('hello');
+        }
+    });
+
